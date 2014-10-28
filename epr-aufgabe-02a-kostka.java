fd5b5219b5e12b38c7e47a7a174dfbda82046256
Sedrix <Sedrix@users.noreply.github.com>
2014-10-28 19:45:15 +0100

    Update epr-aufgabe-02a-kostka.java

--- epr-aufgabe-02a-kostka.java
+++ epr-aufgabe-02a-kostka.java
@@ -1,118 +1,31 @@
 /** Author: Sebastian Kostka
-  * Diese Klasse enthält Ausdrücke mit Berechnungen, die unter
-  * Verwendung von Variablen durchgeführt werden.
+  * Diese Klasse enthält Ausdrücke, zur Durchführung von Prozentrechnungen.
   */
 
-public class Zinsberechnung {
+public class Prozentrechnungen {
     
-    /** 
-      * @param args wird nicht verwendet
-      */
+     /** 
+       * @param args wird nicht verwendet
+       */
     
     public static void main(String[] args) {
         
-        /* Berechnung eines Betrags ("betrag1"), der für drei Jahre
-         * (als "betrag"), zu einem Zinssatz (als "zinssatz") angelegt wird.
-         * Formel: betrag1 = betrag * (1 + zinssatz / 100)
+        /* Berechnung eines Betrags "b1", der ein Jahr zuvor
+         * (als Betrag "b") zu einem Zinssatz "p" angelegt wurde.
+         * Formel: b1 = b * (1 + p / 100)
+         * Umstellung d. Formel: b1 = b * (p + 100) / 100
          */
         
-        /* Deklaration des Betrags, als Variable "betrag",
-         * vom Typ Integer.
+        System.out.println(240 * 103 / 100);
+        System.out.println(80 * 102 / 100);
+        
+        /* Wird der Betrag "b" für zwei Jahre angelegt, erhöht sich "b"
+         * durch Zinsen und Zinseszinsen auf einen Betrag "b2".
+         * Formel: b2 = b * (1 + p / 100) * (1 + p / 100)
+         * Umstellung d. Formel: b2 = b * (p + 100) * (p + 100) / 10000
          */
         
-        int betrag = 117;
-        
-        /* Deklaration des Zinssatzes, als Variable "zinssatz",
-         * vom Typ Integer.
-         */
-        
-        int zinssatz = 3;
-        
-        /* Deklaration der Zinseszinsen, als Variable "zins",
-         * vom Typ Integer.
-         */
-        
-        int zins = (zinssatz + 100);
-        
-        /* Umstellung der Formel für ein Jahr
-         * betrag * (3 + 100) / 100
-         * betrag * (3 + 100) % 100
-         */
-        
-        int euro = betrag * zins / 100;
-        int cent = betrag * zins % 100;
-        
-        // Ausgabe von "betrag1" nach Ablauf eines Jahres.
-        System.out.print(euro);
-        System.out.print(',');
-        System.out.println(cent);
-        
-        /* Umstellung der Formel für zwei Jahre
-         * betrag * (3 + 100) * (3 + 100) / 10000
-         * (betrag * (3 + 100) * (3 + 100) % 10000) / 100
-         */
-        
-        euro = betrag * zins * zins / 10000;
-        cent = betrag * zins * zins % 10000 / 100;
-                
-        // Ausgabe von "betrag1" nach Ablauf von zwei Jahren.
-        System.out.print(euro);
-        System.out.print(',');
-        System.out.println(cent);
-        
-        /* Umstellung der Formel für drei Jahre
-         * betrag * (3 + 100) * (3 + 100) * (3 + 100) / 1000000
-         * (betrag * (3 + 100) * (3 + 100) * (3 + 100) % 1000000) / 10000
-         */
-        
-        euro = betrag * zins * zins * zins / 1000000;
-        cent = betrag * zins * zins * zins % 1000000 / 10000;
-        
-        // Ausgabe von "betrag1" nach Ablauf von drei Jahren.
-        System.out.print(euro);
-        System.out.print(',');
-        System.out.println(cent);
-        
-        
-        /* Ab hier folgt der zweite Aufgabenteil:
-         * Rundung der Beträge auf volle Cent.
-         */
-        
-        // Berechnung für ein Jahr.
-        euro = betrag * zins / 100;
-        cent = betrag * zins % 100 * 100;
-        
-        // Rundung auf volle Cent.
-        cent = (cent + 50) / 100;
-        
-        // Ausgabe von "betrag1", gerundet auf volle Cent, nach einem Jahr.
-        System.out.print(euro);
-        System.out.print(',');
-        System.out.println(cent);
-        
-        // Berechnung für zwei Jahre.
-        euro = betrag * zins * zins / 10000;
-        cent = betrag * zins * zins % 10000;
-        
-        // Rundung auf volle Cent.
-        cent = (cent + 50) / 100;
-        
-        // Ausgabe von "betrag1", gerundet auf volle Cent, nach zwei Jahren.
-        System.out.print(euro);
-        System.out.print(',');
-        System.out.println(cent);
-        
-        // Berechnung für drei Jahre.
-        euro = betrag * zins * zins * zins / 1000000;
-        cent = betrag * zins * zins * zins % 1000000 / 100;
-        
-        // Rundung auf volle Cent.
-        cent = (cent + 50) / 100;
-        
-        // Ausgabe von "betrag1", gerundet auf volle Cent, nach drei Jahren.
-        System.out.print(euro);
-        System.out.print(',');
-        System.out.println(cent);
+        System.out.println(240 * (107) * (107) / 10000);
         
     }
     
