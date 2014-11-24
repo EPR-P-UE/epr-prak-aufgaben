1dbb57dd3632e78a1ee0a74dd6a6e82da7ddfab0
LordOfBugs <LordOfBugs@users.noreply.github.com>
2014-11-24 20:06:11 +0100

    Update epr-aufgabe-06-lordofbugs.java

--- epr-aufgabe-06-lordofbugs.java
+++ epr-aufgabe-06-lordofbugs.java
@@ -1,6 +1,6 @@
 /** Diese Klasse bearbeitet die Aufgaben in der Praktikumsaufgaben 6.
  * Dabei handelt es sich um Rekursiv gelöste Zahlenspielereien
- * @author LodOfBugs
+ * @author Rafael Martin
  */
 public class Zahlenspielerei {
     
@@ -11,16 +11,19 @@ public class Zahlenspielerei {
      * @param args wird nicht verwendet
      */
     public static void main(String []args) {
+        //Testfälle Aufgabe 1
         System.out.println(enthaelt(4711, 1));
         System.out.println(enthaelt(4711, 2));
         System.out.println(enthaelt(789, 9));
         System.out.println(enthaelt(0, 0));
-       
+        
+       //Testfälle Aufgabe 2
         System.out.println(gibAnzahl(4711, 1));
         System.out.println(gibAnzahl(4711, 2));
         System.out.println(gibAnzahl(789, 9));
         System.out.println(gibAnzahl(0, 0));
         
+        //Testfälle Aufgabe 3
         System.out.println(filter(1332, 3));
         System.out.println(filter(3333, 3));
         System.out.println(filter(1332, 4));
@@ -40,11 +43,19 @@ public class Zahlenspielerei {
      */
     public static boolean enthaelt(long zahl, int ziffer) {
 
+        //Wenn die letzte Stelle = Ziffer ist, dann true
         return (zahl % 10 == ziffer
             ? true
+            /* sonst wird geprüft ob es bereits die letzte Stelle ist
+                * Ist dem so, dann ist die Angabe false
+                */
             : zahl <= 9
                 ? false
-                : enthaelt(zahl / 10, ziffer);
+                /* SONST: rufe die Methode erneut auf
+                * Die Zahl  eine Stelle kürzen und sie sowieo Ziffer übergeben
+                * Dann von oben: erneut prüfen bis letzte Stelle erreicht ist.
+                */ 
+                : enthaelt(zahl / 10, ziffer));
     }
     
     /**
@@ -57,27 +68,54 @@ public class Zahlenspielerei {
     
     public static int gibAnzahl(long zahl, int ziffer) {
         int ausgabeWert;
-        
+    //Wenn die Zahl kleiner als 9 ist
         ausgabeWert = zahl <= 9
+                //dann wird direkt auf die Ziffer geprüft
                 ? zahl == ziffer
+                //falls ja, wird einmal hochgezählt
                     ? +1
+                //sonst nicht
                     : +0
+                //Sonst: Methode neu aufrufen mit einer Stelle versetzt
                 : gibAnzahl(zahl / 10, ziffer)
+                //Die abgeschnittene Stelle wird auf die Ziffer geprüft
                 + (zahl % 10 == ziffer
+                //Falls es sie ist: Hochzählen.
                         ? +1
+                //Sonst: Nichts
                         : +0);
         
         return (ausgabeWert);
     }
     
+    /**
+     * Bei dieser Aufgabe ist eine Zahl und eine Ziffer gegeben.
+     * Es sollen Stellen entsprechend der Ziffer gestrichen werden.
+     * @param zahl 
+     * @param ziffer 
+     * @return zahl was auch immer
+     */ 
     public static long filter(long zahl, int ziffer) {
-        
+        //Zuerst wird geprüft ob beriets die letzte Ziffer vorliegt
         return (zahl <= 9 
-                ? zahl % 10 == ziffer
+                //Falls ja: wird geprüft ob die Zahl = Ziffer ist
+                ? zahl == ziffer
+                //Falls ja wird sie gelöscht (0 gesetzt)
                     ? 0
-                    : zahl % 10
+                //Falls nein, bleibt sie erhalten.
+                    : zahl
+                //Bei Mehrstelligkeit wird die hinterste Stelle abgeschnitten
+                //Und auf Ziffergleichheit geprüft
                 : zahl % 10 == ziffer
+                /* Sollte dem so sein so wird die Stelle abgeschnitten
+                * Die verkürzte Zahl wird samt der Ziffer übergeben
+                */
                     ? filter(zahl / 10, ziffer)
-                    : 10 * filter (zahl / 10, ziffer) + (zahl % 10));
+                /* Ansonsten wird die Stelle wieder angehängt
+                 * und der Methode für die nächste Prüfung übergeben
+                 * Da die Prüfung eher stattfindet werden die Stellen erst
+                 * beim Zurückgehen (rekursiv) angehängt
+                */
+                    : (zahl % 10) + filter(zahl / 10, ziffer) * 10);
     }
 }
